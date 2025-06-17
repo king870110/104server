33d9f69edb975dfcb16482396761949f1a3c16d5
king870110 <king870110@gmail.com>
2025-06-17 21:07:12 +0800

    更新last_sync.json中的同步日期至2025-06-16，并在supabase_client.py中添加打印SUPABASE_URL和SUPABASE_KEY的调试信息

--- supabase_client.py
+++ supabase_client.py
@@ -7,6 +7,9 @@ load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 TABLE = "jobs"
+print("SUPABASE_URL =", SUPABASE_URL)
+print("SUPABASE_KEY =", SUPABASE_KEY)
+
 
 headers = {
     "apikey": SUPABASE_KEY,
