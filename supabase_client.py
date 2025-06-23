5ec1b3972efd4bec528a3d06d375db28687b9432
king870110 <king870110@gmail.com>
2025-06-23 20:42:01 +0800

    allow change github json

--- supabase_client.py
+++ supabase_client.py
@@ -8,8 +8,8 @@ load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 TABLE = "jobs"
-print("SUPABASE_URL =", SUPABASE_URL)
-print("SUPABASE_KEY =", SUPABASE_KEY)
+# print("SUPABASE_URL =", SUPABASE_URL)
+# print("SUPABASE_KEY =", SUPABASE_KEY)
 
 
 headers = {
